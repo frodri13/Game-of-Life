de771a7d6b5a34287c3a31568cd1a6e82bac3eac
Fabio Rodriguez <alessf13@gmail.com>
2023-05-08 11:03:08 +0100

    grid by 50x50 created

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,23 @@
-const Grid = () => {
+'use client'
 
+import { useState } from "react";
+
+const numRows = 50;
+const numCols = 50;
+
+const Grid = () => {
+    const [grid, setGrid] = useState(() => {
+        const rows = [];
+
+        for(let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => 0));
+        }
+
+        return rows;
+    });
+
+    console.log(grid);
+    
     return(
         <div>Grid</div>
     )
