4e03b484e3e9a2ea895c9ebd7e028360fa704436
Fabio Rodriguez <alessf13@gmail.com>
2023-05-08 14:27:31 +0100

    simulation running

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,19 @@
 'use client'
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 const numRows = 12;
 const numCols = 12;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]
 
 const Grid = () => {
   const [grid, setGrid] = useState(() => {
@@ -23,20 +34,87 @@ const Grid = () => {
     setGrid(newGrid);
   };
 
+  const [running, setRunning] = useState(false)
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if(!runningRef.current) {
+      return;
+    }
+
+    const newGrid = [...grid];
+    
+    for(let i = 0; i < numCols; i++){
+      for(let j = 0; j < numRows; j++) {
+        let neighbors = 0;
+        operations.forEach(([x,y]) => {
+          const newI = i + x;
+          const newJ = j + y;
+          if(newI >= 0 && newI < numCols && newJ >= 0 && newJ < numRows ){
+            neighbors += grid[newI][newJ];
+          }
+        })
+
+        if(neighbors < 2 || neighbors > 3) {
+          newGrid[i][j] = 0;
+        } else if (grid[i][j] === 0 && neighbors === 3) {
+          newGrid[i][j] = 1; 
+        }
+      }
+    }
+    setGrid(newGrid);
+
+    setTimeout(runSimulation, 1000);
+  }, [])
+  // function game() {
+  //   while(running == true) {
+  //     for(let i = 0; i < numCols; i++){
+  //       for(let j = 0; j < numRows; j++) {
+  //         let cell = grid[i][j];
+  //         const cellNeighbors = [
+  //           grid[i + 1][j], 
+  //           grid[i][j + 1], 
+  //           grid[i + 1][j + 1], 
+  //           grid[i -1][j], 
+  //           grid[i][j - 1], 
+  //           grid[i -1][j - 1], 
+  //           grid[i - 1][j], 
+  //           grid[i + 1][j - 1], 
+  //         ]
+  //         cellNeighbors.map((c) => c = 1)
+  //       }
+  //     }
+  //   }
+  // }
+
+
+
   return (
-    <div className="grid grid-cols-12 gap-0 m-0 p-0">
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <div
-            key={`${i}-${k}`}
-            onClick={() => handleCellClick(i, k)}
-            className={`w-20 h-10 ${
-              grid[i][k] ? "bg-purple-500" : ""
+    <>
+        <button onClick={() =>{
+            setRunning(!running);
+            runningRef.current = true;
+            runSimulation();
+        }}
+        >
+          {running ? 'Stop' : 'Start'}
+          </button>
+        <div className="grid grid-cols-12 gap-0 m-0 p-0">
+        {grid.map((rows, i) =>
+            rows.map((col, k) => (
+            <div
+                key={`${i}-${k}`}
+                onClick={() => handleCellClick(i, k)}
+                className={`w-20 h-10 ${
+                grid[i][k] ? "bg-purple-500" : ""
             } border border-black`}
-          />
+        />
         ))
-      )}
+    )}
     </div>
+    </>
+   
   );
 };
 
