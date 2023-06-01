e6926cde7eeb9fdffdbf7bc55d89de90c744428e
Fabio Rodriguez <alessf13@gmail.com>
2023-06-01 12:05:05 +0100

    added remaining buttons into grid

--- components/ButtonGrid.tsx
+++ components/ButtonGrid.tsx
@@ -1,11 +1,15 @@
 import DarkModeToggle from "./DarkModeToggle";
+import MinusButton from "./MinusButton";
 import PausePlay from "./PausePlay";
+import PlusButton from "./PlusButton";
 
 export default function ButtonGrid(){
     return(
         <div>
             <DarkModeToggle /> 
             <PausePlay />
+            <PlusButton />
+            <MinusButton />
         </div>
     )
 }
