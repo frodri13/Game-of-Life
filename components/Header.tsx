6c4a05487ba26bb24fff2df9bafb0fa58df6827b
Fabio Rodriguez <alessf13@gmail.com>
2023-06-02 05:06:57 +0100

    title added

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
 
     return(
         <header>
+            <h1 className="text-center mt-6 text-blue-500 dark:text-pink-500 font-bold text-4xl font-mono uppercase tracking-[20px]">Game of Life</h1>
             <ButtonGrid />
         </header>
     )
